3b5007ba0041c76da61a7eca5751d59bc3bc8008
Mikko Ropanen <mropanen@gmail.com>
2014-03-26 01:07:25 +0200

    Initial commit

---
+++ app/src/main/java/fi/bunnyfeet/servicemonitor/app/CheckService.java
@@ -0,0 +1,84 @@
+package fi.bunnyfeet.servicemonitor.app;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class CheckService extends Service {
+    public static String CHECK = "fi.bunnyfeet.servicemonitor.CHECK";
+    public static int OK = 1;
+    public static int DOWN = 2;
+    public static int BAD_HOST = 3;
+
+    public CheckService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service", "onstartcommand");
+
+        return START_STICKY;
+    }
+
+    public int checkUrl(String url) {
+        int status = -1;
+
+        new Checker(this).execute(url);
+
+        return status;
+    }
+
+    private class Checker extends AsyncTask<String, Integer, Integer> {
+        private Context checkerContext;
+
+        public Checker(Context context) {
+            checkerContext = context;
+        }
+
+        protected Integer doInBackground(String... urls) {
+            int status = -1;
+
+            HttpGet req = new HttpGet(urls[0]);
+            HttpClient client = new DefaultHttpClient();
+            try {
+                HttpResponse response = client.execute(req);
+                Log.d("response", "response code: " + response.getStatusLine().getStatusCode());
+
+                int responseCode = response.getStatusLine().getStatusCode();
+
+                Intent intent = new Intent(CHECK);
+                intent.putExtra("Url", urls[0]);
+                intent.putExtra("Status", "status: " + responseCode);
+                LocalBroadcastManager.getInstance(checkerContext).sendBroadcast(intent);
+
+                return responseCode;
+            }
+            catch(UnknownHostException e) {
+                return CheckService.BAD_HOST;
+            }
+            catch (Exception e) {
+                Log.d("fail", e.toString());
+                return -1;
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/fi/bunnyfeet/servicemonitor/app/MainActivity.java
@@ -0,0 +1,74 @@
+package fi.bunnyfeet.servicemonitor.app;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.content.LocalBroadcastManager;
+
+
+public class MainActivity extends Activity {
+
+    private CheckResponseReceiver checkResponseReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CheckService.class);
+        startService(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (checkResponseReceiver == null) {
+            checkResponseReceiver = new CheckResponseReceiver();
+        }
+       LocalBroadcastManager.getInstance(this).registerReceiver(checkResponseReceiver, new IntentFilter(CheckService.CHECK));
+    }
+    protected void onPause() {
+        super.onPause();
+        if (checkResponseReceiver != null) {
+            unregisterReceiver(checkResponseReceiver);
+        }
+    }
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(checkResponseReceiver);
+        super.onDestroy();
+    }
+
+    private class CheckResponseReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("receiver", intent.getStringExtra("Url") + ": " + intent.getStringExtra("Status"));
+        }
+    }
+
+}
